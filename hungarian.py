25e314f345d2e29d8ee115b42455a883136015b1
Fahad Almana <falmana@barzanholdings.com>
2022-06-29 16:38:16 +0300

    reached page 14 in slides

--- hungarian.py
+++ hungarian.py
@@ -4,12 +4,12 @@ class HungarianAlgorithm():
     def __init__(self,array) -> None:
         
         self.array_original = array
-        self.marked_posiitons = np.full((array.shape), False)
+        self.marked_cells = []
         self.array_current = np.copy(self.array_original)
         self.num_lines = 0
         self.zero_condition()
-        print(self.array_original)
-        print(self.array_current)
+        self.least_zero_row()
+        print(self.marked_cells)
         print(self.bool_mat)
     
     def row_reduction(self):
@@ -39,6 +39,47 @@ class HungarianAlgorithm():
     # creates a boolean matrix of current array with 0's as True
     def boolean_matrix(self):
         self.bool_mat = (self.array_current == 0)
+
+    def least_zero_row(self):
+        # find the row with the least trues
+        # if the bool matrix is fully false go to a different step
+        if np.array_equal(self.bool_mat , np.full(self.bool_mat.shape, False)):
+            self.bool_mat = np.full(self.bool_mat.shape, False)
+            for i in self.marked_cells:
+                self.bool_mat[i[0],i[1]] = True
+            return
+        min_val = self.bool_mat.shape[0]
+        min_idx = None
+        for i, row in enumerate(self.bool_mat):
+            
+            if sum(row) == 0:
+                continue
+            elif sum(row) < min_val:
+                min_val = sum(row)
+                min_idx = i
+                if sum(row) == 1:
+                    return self.mark_elements(min_idx)
+        return self.mark_elements(min_idx)
+    
+    def mark_elements(self, min_idx):
+        if min_idx is None:
+            self.bool_mat = np.full(self.bool_mat.shape, False)
+            for i in self.marked_cells:
+                self.bool_mat[i[0],i[1]] = True
+            return
+        for column, value in enumerate(self.bool_mat[min_idx]):
+            if value == True:
+                self.marked_cells.append([min_idx, column])
+                self.clean_up()
+
+    def clean_up(self):
+        for i in self.marked_cells:
+            row = i[0]
+            column = i[1]
+            self.bool_mat[row] = False
+            self.bool_mat[:,column] = False
+        self.least_zero_row()
+
     
 
 if __name__ == "__main__":
